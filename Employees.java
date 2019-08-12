b65a4dc4360d84d796773f4fac7ec5d6400990a5
Bulgakoff <hlbu@yandex.ru>
2019-08-12 08:11:33 +0300

    сделал метод

--- Employees.java
+++ Employees.java
@@ -1,4 +1,22 @@
 package GBTheme4oopRazbor;
 
 public class Employees {
+    private String fio;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private int salary;
+    private int age;
+
+    public Employees(String fio, String position, String email, int phoneNumber, int salary, int age){
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+    void doInfoEmployees() {
+        System.out.println(age + " - летний Сотрудник " + fio + "на должности " + position + "\nс зарплатой " + salary + " руб/день " + " номер телефона и e-mail :" + phoneNumber + "," + email);
+    }
 }
