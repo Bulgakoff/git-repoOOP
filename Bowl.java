806de49e2ff5a5ef8aac04bf13f86dbbc5e41e43
Bulgakoff <hlbu@yandex.ru>
2019-08-15 09:09:30 +0300

    добавил класс миска

--- Bowl.java
+++ Bowl.java
@@ -1,4 +1,11 @@
 package GBTheme4oopRazbor;
 
 public class Bowl {
+    int volume;
+    public  Bowl(int volume){
+        this.volume=volume;
+    }
+    public void fullBowl(){
+        System.out.println();
+    }
 }
